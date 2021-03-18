a0a9c8154076f29049b479665ff25dae0339956d
Akvilion <ianko.andrii@gmail.com>
2021-03-18 08:20:08 +0200

    fix admin

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
 from . import models
 from django import forms
+from django.forms import ModelChoiceField
 
 # Register your models here.
 
 
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-    pass
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(models.Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class NotebookAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(models.Category.objects.filter(slug='notebooks'))
+            return ModelChoiceField(models.Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(models.Category)
 admin.site.register(models.Notebook, NotebookAdmin)
-admin.site.register(models.Smartphone)
+admin.site.register(models.Smartphone, SmartphoneAdmin)
 admin.site.register(models.CartProduct)
 admin.site.register(models.Cart)
 admin.site.register(models.Customer)

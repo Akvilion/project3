80da790cdda6cf326ccc9bb1b653d12111dd66f6
Akvilion <ianko.andrii@gmail.com>
2021-03-23 21:25:00 +0200

    add validation for min image

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from . import models
 from django import forms
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm
+from PIL import Image
 
 # Register your models here.
+
+
+class NotebookAdminForm(ModelForm):
+
+    MIN_RESOLUTION = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = 'Завантажуйте зображення з мінімальним розширенням {} x {}'.format(*self.MIN_RESOLUTION)
+    
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        if img.height < min_height or img.widrh < min_width:
+            raise ValidationError('Розширення картинки менше мінімального!')
+        return image
+
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
@@ -15,6 +34,8 @@ class SmartphoneAdmin(admin.ModelAdmin):
 
 
 class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
